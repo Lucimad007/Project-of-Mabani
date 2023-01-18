da8e2b27851c218da9d190e9d33cb0c4a1d8043f
Seyyed Mohammad Fatemi <smf007smf007@gmail.com>
2023-01-18 16:53:51 +0330

    Select Level Menu has been created

--- main.c
+++ main.c
@@ -5,6 +5,8 @@
 #include "helper_windows.h"
 #include "colorize.h"
 #define clear() system("cls")
+enum Difficulity {EASY, MEDIUM, HARD};
+typedef enum Difficulity Difficulity;
 struct User{
     char name[20];
     char family[20];
@@ -13,10 +15,18 @@ struct User{
     char password[20];
 };
 typedef struct User User;
+struct Data{
+    char nickName[20];
+    int score;
+    time_t date;
+    Difficulity level;
+};
+typedef struct Data Data;
 
 enum situation {MAIN_MENU, SIGN_IN_MENU, REGISTER_MENU, SELECT_LEVEL, GAME};
 typedef enum situation situation;
 
+User user;
 situation flag = MAIN_MENU;
 FILE* users;
 
@@ -25,11 +35,19 @@ void my_callback_on_key_arrival(char c);
 void MainMenu();
 void SignInMenu();
 void Register();
+void selectLevelMenu();
 void createFilesOfWords();
 
 int main()
 {
    createFilesOfWords();
+   //Data data1,data2,data3;
+   //data1.score = 300; data2.score = 400; data3.score =350; FILE *file = fopen("ali.bin","wb"); data1.level = HARD;
+  // data2.level = MEDIUM; data3.level = EASY;data1.date = time(NULL);data2.date = time(NULL);data3.date = time(NULL);
+  // fwrite(&data1, sizeof(Data), 1, file);
+  // fwrite(&data2, sizeof(Data), 1, file);
+   //fwrite(&data3, sizeof(Data), 1, file);
+   //fclose(file);
   //Example codes.
   MainMenu();
   //clear();
@@ -53,9 +71,9 @@ void my_callback_on_key_arrival(char c)
             flag = REGISTER_MENU;
             Register();
         }
-    } else if(flag == SIGN_IN_MENU){
-        //SignInMenu();
     }
+
+
 }
 ////////////////////////////////////////////////////////////
 void MainMenu(){
@@ -141,15 +159,17 @@ void SignInMenu(){
     gotoxy(0,(HEIGHT)+2);
     FILE* file = fopen("users.bin","rb");
     if(file != NULL){
-        User user;
+        User temp;
         while(!feof(file)){
-            fread(&user, sizeof(User), 1, file);
-            if(strcmp(user.nickName , Name) == 0)
-                if(strcmp(user.password , Password) == 0)
+            fread(&temp, sizeof(User), 1, file);
+            if(strcmp(temp.nickName , Name) == 0)
+                if(strcmp(temp.password , Password) == 0)
                     {
-                        flag = GAME;
+                        user = temp;
+                        flag = SELECT_LEVEL;
                         clear();
                         fclose(file);
+                        selectLevelMenu();
                         return;
                     }
         }
@@ -162,7 +182,7 @@ void SignInMenu(){
 }
 ////////////////////////////////////////////////////////////
 void Register(){
-    User user;
+    User temp;
     int WIDTH = 32;
     int HEIGHT = 28;
 
@@ -208,27 +228,110 @@ void Register(){
 //    printf("%s",str4);
 
     gotoxy((WIDTH-strlen(str1))/2,(HEIGHT)/2 - 3);
-    gets(user.name);
+    gets(temp.name);
     gotoxy((WIDTH-strlen(str2))/2,(HEIGHT)/2 - 1);
-    gets(user.family);
+    gets(temp.family);
     gotoxy((WIDTH-strlen(str3))/2,(HEIGHT)/2 + 1);
-    scanf("%d",&user.age);
+    scanf("%d",&temp.age);
     getchar();             // to get rid of \n
     gotoxy((WIDTH-strlen(str4))/2,(HEIGHT)/2 + 3);
-    gets(user.nickName);
+    gets(temp.nickName);
     gotoxy((WIDTH-strlen(str5))/2,(HEIGHT)/2 + 5);
-    gets(user.password);
+    gets(temp.password);
 
 
 
     gotoxy(0,(HEIGHT)+2);
     FILE* file = fopen("users.bin","ab");
-    fwrite(&user, sizeof(User), 1, file);
+    fwrite(&temp, sizeof(User), 1, file);
 
+    user = temp;
+    flag = SELECT_LEVEL;
     clear();
     fclose(file);
+    selectLevelMenu();
     return;
 }
+////////////////////////////////////////////////////////////
+void selectLevelMenu(){
+    int WIDTH = 66;
+    int HEIGHT = 24;
+    char str1[8] = "1. Easy";
+    char str2[10] = "2. Medium";
+    char str3[8] = "3. Hard";
+    char str4[21] = "<Select Level Menu>";
+    char str5[17] = "previous games :";
+
+    setcolor(5);
+    for(int i=1;i<=HEIGHT;i++)
+    {
+        for(int j=1;j<=WIDTH;j++)
+        {
+            if((j == 1) || (j == WIDTH) || (i == HEIGHT) || (i == 1))
+            {
+                 printf("%c",(char)6);
+            } else
+                printf(" ");
+        }
+        printf("\n");
+    }
+
+    setcolor(4);
+    gotoxy((WIDTH-strlen(str4))/2 , HEIGHT/2 -10);
+    printf("%s",str4);
+
+    setcolor(7);
+    gotoxy((WIDTH-strlen(str1))/2 , HEIGHT/2 - 8);
+    printf("%s",str1);
+    gotoxy((WIDTH-strlen(str2))/2 , HEIGHT/2 - 6);
+    printf("%s",str2);
+    gotoxy((WIDTH-strlen(str3))/2 , HEIGHT/2 - 4);
+    printf("%s",str3);
+
+    setcolor(4);
+    gotoxy((WIDTH-strlen(str5))/2 , HEIGHT/2 - 2);
+    printf("%s",str5);
+
+    FILE* file = fopen(strcat(user.nickName,".bin"),"rb");
+    if((file == NULL) || (file == feof(file))){
+        setcolor(7);
+        char message[12] = "No History!";
+        gotoxy((WIDTH-strlen(message))/2 , HEIGHT/2 );
+        printf("%s", message);
+        gotoxy(0,HEIGHT+3);
+    } else {
+        setcolor(7);
+        Data data;
+        gotoxy(3, HEIGHT/2 + 2);
+        printf("Score(s)");
+        gotoxy(15, HEIGHT/2 + 2);
+        printf("date(s)");
+        gotoxy(46, HEIGHT/2 + 2);
+        printf("Difficulity");
+
+        for(int i=1;i<=3;i++){
+            if(feof(file))
+                break;
+            fread(&data ,sizeof(Data), 1, file);
+            gotoxy(3, HEIGHT/2 + i*2 + 2);
+            printf("%d. %d",i,data.score);
+            gotoxy(15, HEIGHT/2 + i*2 + 2);
+            char *date = ctime(&data.date);
+            printf("%s",date);
+            gotoxy(46, HEIGHT/2 + i*2 + 2);
+            if(data.level == EASY)
+                printf("easy");
+            else if(data.level == MEDIUM)
+                printf("medium");
+            else if(data.level == HARD)
+                printf("hard");
+        }
+    }
+
+    gotoxy(0,HEIGHT+3);
+    fclose(file);
+}
+////////////////////////////////////////////////////////////
 void createFilesOfWords(){
     FILE* file = fopen("normal.txt","w");
     char arr[43] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9','0','_','@','%','$','^','&','!'};
