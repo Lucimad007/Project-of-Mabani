f19122c5046cd66c061538329a1b1c777277ff5d
Seyyed Mohammad Fatemi <smf007smf007@gmail.com>
2023-01-22 19:52:09 +0330

    main loop of game has been created

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <unistd.h>
+#include <stdbool.h>
 #include "helper_windows.h"
 #include "colorize.h"
 #define clear() system("cls")
@@ -32,7 +34,7 @@ struct Data{
 };
 typedef struct Data Data;
 
-enum situation {MAIN_MENU, SIGN_IN_MENU, REGISTER_MENU, START_MENU, SELECT_LEVEL, GAME};
+enum situation {MAIN_MENU, SIGN_IN_MENU, REGISTER_MENU, START_MENU, SELECT_LEVEL, GAME, IN_GAME};
 typedef enum situation situation;
 
 User user;
@@ -40,9 +42,19 @@ situation flag = MAIN_MENU;
 FILE* users;
 NodePtr head = NULL;
 int countOfWords = 0;
+int y = 0;   //even empty lines
+int width = 44;
+int height = 26;
 int Current_Score = 0;
 double Time_Period = 0;
 int Wave = 0;
+int Word_Counter = 0;    //for counting numbers to determine when we should create a new wave
+int Char_Situation = 0;
+int SizeOfCurrentWord = 0;
+int colors[20];
+Difficulity curLevel;
+int IN_PROGRESS = 0;
+int flag_sit = 0;
 
 void my_callback_on_key_arrival(char c);
 
@@ -52,7 +64,14 @@ void Register();
 void selectLevelMenu();
 void createFilesOfWords();
 void startButton();
-void startGame();
+void createDisplay();
+void updateDisplay();
+void resetColor();
+void clearDisplay();
+void updateHead();
+void clearHead();
+void newWave();
+//void isMobham();
 NodePtr createNode();
 void addWord(char word[]);
 void deleteWord();
@@ -60,29 +79,36 @@ void deleteWord();
 int main()
 {
    createFilesOfWords();
- //  addWord("ali");
- //  addWord("mmd");
-  // addWord("hole");
-  // deleteWord();
-  // addWord("last");
- //  while(head!=NULL){
-    //    printf("%s\n",head->word);
-   //     head = head->next;
-  // }
-//printf("counts: %d\n", countOfWords);
-  // Data data1,data2,data3;
- // data1.score = 300; data2.score = 400; data3.score =350; FILE *file = fopen("ali.bin","wb"); data1.level = HARD;
-  // data2.level = MEDIUM; data3.level = EASY;data1.date = time(NULL);data2.date = time(NULL);data3.date = time(NULL);
-  // fwrite(&data1, sizeof(Data), 1, file);
-  //fwrite(&data2, sizeof(Data), 1, file);
-  // fwrite(&data3, sizeof(Data), 1, file);
-   //fclose(file);
-  //Example codes.
+
+    for(int i =0;i<10;i++)
+        addWord("ali");
    MainMenu();
-  //clear();
-  HANDLE thread_id = start_listening(my_callback_on_key_arrival);
+    HANDLE thread_id = start_listening(my_callback_on_key_arrival);
 
+    while(1){
+        if(flag!=IN_GAME)
+            continue;
+        while(IN_PROGRESS){}
+        IN_PROGRESS = 1;
+        clearDisplay();
+        y++;
+        updateDisplay();
+        IN_PROGRESS = 0;
+        Word_Counter++;
 
+        if(Word_Counter == 10){
+            newWave();
+            if(curLevel == HARD)
+                Time_Period *= 0.6;
+            else if(curLevel == MEDIUM)
+                Time_Period *= 0.7;
+            else if(curLevel == EASY)
+                Time_Period *= 0.8;
+            Word_Counter = 0;
+        }
+
+        usleep((Time_Period/10)*10*10*10*10*10*10);   //   /10 because words of waves are 10 and * 10^6 because it should be micro sec
+    }
 
   WaitForSingleObject(thread_id, INFINITE);
   return 0;
@@ -90,6 +116,8 @@ int main()
 
 void my_callback_on_key_arrival(char c)
 {
+    if(flag_sit)
+        return;
     if(flag == MAIN_MENU){
         if(c == '1'){
             clear();
@@ -112,6 +140,7 @@ void my_callback_on_key_arrival(char c)
                 Wave = 1;
                 Current_Score = 0;
                 Time_Period = 10;
+                curLevel = EASY;
                 flag = START_MENU;
                 clear();
                 startButton();
@@ -120,6 +149,7 @@ void my_callback_on_key_arrival(char c)
                 Wave = 1;
                 Current_Score = 0;
                 Time_Period = 8;
+                curLevel = MEDIUM;
                 flag = START_MENU;
                 clear();
                 startButton();
@@ -128,6 +158,7 @@ void my_callback_on_key_arrival(char c)
                 Wave = 1;
                 Current_Score = 0;
                 Time_Period = 5;
+                curLevel = HARD;
                 flag = START_MENU;
                 clear();
                 startButton();
@@ -140,6 +171,7 @@ void my_callback_on_key_arrival(char c)
                     fread(&tempData, sizeof(Data), 1, temp);
                     Wave = 1;
                     Current_Score = tempData.score;
+                    curLevel = tempData.level;
                     if(tempData.level == EASY)
                         Time_Period = 10;
                     else if(tempData.level == MEDIUM)
@@ -160,6 +192,7 @@ void my_callback_on_key_arrival(char c)
                     fread(&tempData, sizeof(Data), 1, temp);
                     Wave = 1;
                     Current_Score = tempData.score;
+                    curLevel = tempData.level;
                     if(tempData.level == EASY)
                         Time_Period = 10;
                     else if(tempData.level == MEDIUM)
@@ -180,6 +213,7 @@ void my_callback_on_key_arrival(char c)
                     fread(&tempData, sizeof(Data), 1, temp);
                     Wave = 1;
                     Current_Score = tempData.score;
+                    curLevel = tempData.level;
                     if(tempData.level == EASY)
                         Time_Period = 10;
                     else if(tempData.level == MEDIUM)
@@ -197,9 +231,37 @@ void my_callback_on_key_arrival(char c)
         }
 
     }else if(flag == GAME){
-        startGame();
+        createDisplay();
+        flag = IN_GAME;
+    } else if((flag == IN_GAME)&&(head!=NULL)&&(y>1)){
+        SizeOfCurrentWord = strlen(head->word);
+        char word[20];
+        strcpy(word, head->word);
+        if(c == word[Char_Situation])
+            colors[Char_Situation] = 1;
+        else
+            colors[Char_Situation] = -1;
+        Char_Situation++;
+
+        if(Char_Situation == SizeOfCurrentWord){
+            while(IN_PROGRESS){}
+            IN_PROGRESS = 1;
+            clearHead();
+            Char_Situation = 0;
+            resetColor();
+            clearDisplay();
+            deleteWord();
+            updateDisplay();
+            IN_PROGRESS = 0;
+        } else{
+            while(IN_PROGRESS){}
+            IN_PROGRESS = 1;
+            updateHead();
+            IN_PROGRESS = 0;
+        }
     }
 
+    //usleep(200000);     //to prevent program from some bugs and disallow user to cheat
 
 }
 ////////////////////////////////////////////////////////////
@@ -478,31 +540,159 @@ void startButton(){
     }
 
     setcolor(7);
-    char str[22] = "Press Enter to Start!";
+    char str[24] = "Press any key to Start!";
     gotoxy((WIDTH-strlen(str))/2, HEIGHT/2);
     printf("%s",str);
     flag = GAME;
     return;
 }
-void startGame(){
-    clear();
-    int WIDTH = 36;
-    int HEIGHT = 26;
-
-    setcolor(7);
-    for(int i=1;i<=HEIGHT;i++)
-    {
-        for(int j=1;j<=WIDTH;j++)
-        {
-            if((j == 1) || (j == WIDTH) || (i == HEIGHT) || (i == 1))
-            {
-                 printf("%c",(char)6);
-            } else
-                printf(" ");
-        }
-        printf("\n");
+void clearHead(){
+    int len = strlen(head->word);
+    gotoxy((width-len)/2 , y);
+    setcolor(0);
+    printf("%s",head->word);
+}
+void resetColor(){
+    for(int i = 0;i<20;i++)
+        colors[i] = 0;
+}
+void clearDisplay(){
+    int temp = y;
+    NodePtr tempPtr = head;
+    setcolor(0);
+    while(temp>0 && tempPtr != NULL){
+        gotoxy((width-strlen(tempPtr->word))/2 , temp);
+        printf("%s",tempPtr->word);
+        temp--;
+        tempPtr = tempPtr->next;
     }
+}
+void updateDisplay(){
+    if(head==NULL)
+        return;
+    updateHead();
+    int temp = y-1;
+    NodePtr tempPtr = head->next;
+    setcolor(7);
+    while(temp>0 && tempPtr != NULL){
+        gotoxy((width-strlen(tempPtr->word))/2 , temp);
+        printf("%s",tempPtr->word);
+        temp--;
+        tempPtr = tempPtr->next;
+    }
+}
+void  updateHead(){
+    if(head == NULL)
+        return;
+    int len = strlen(head->word);
+    gotoxy((width-len)/2 , y);
+    for(int i=0;i<len;i++){
+        if(colors[i] == 1)
+            setcolor(2);
+        if(colors[i] == 0)
+            setcolor(7);
+        if(colors[i] == -1)
+            setcolor(1);
+        printf("%c",head->word[i]);
+    }
+}
+void createDisplay(){
+    clear();
+    setcolor(7);
+    for(int i=0;i<=width;i++)
+    {
+        gotoxy(i, 0);
+        printf("%c",(char)4);
+        gotoxy(i, height);
+        printf("%c",(char)4);
+    }
+    for(int i=0;i<=height;i++)
+    {
+        gotoxy(0, i);
+        printf("%c",(char)4);
+        gotoxy(width, i);
+        printf("%c",(char)4);
+    }
+}
 
+void newWave(){
+    if(Wave == 5)
+        return;
+    Wave++;
+    FILE* file ;
+    srand(time(NULL));
+    int x;
+    char word[20];
+    switch (Wave){
+        case 1:
+            file = fopen("normal.txt","r");
+            x = rand()%100;
+            for(int i=0;i<10;i++){
+                for(int j=0;j<x;j++){
+                    fscanf(file,"%s", word);
+                }
+                addWord(word);
+                x = rand()%100;
+                rewind(file);
+            }
+            fclose(file);
+            break;
+        case 2:
+            file = fopen("hard.txt","r");
+            x = rand()%100;
+            for(int i=0;i<10;i++){
+                for(int j=0;j<x;j++){
+                    fscanf(file,"%s", word);
+                }
+                addWord(word);
+                x = rand()%100;
+                rewind(file);
+            }
+            fclose(file);
+            break;
+        case 3:
+            file = fopen("long.txt","r");
+            x = rand()%100;
+            for(int i=0;i<10;i++){
+                for(int j=0;j<x;j++){
+                    fscanf(file,"%s", word);
+                }
+                addWord(word);
+                x = rand()%100;
+                rewind(file);
+            }
+            fclose(file);
+            break;
+        case 4:
+            file = fopen("long.txt","r");
+            x = rand()%100;
+            for(int i=0;i<10;i++){
+                for(int j=0;j<x;j++){
+                    fscanf(file,"%s", word);
+                }
+                addWord(word);
+                x = rand()%100;
+                rewind(file);
+            }
+            fclose(file);
+            break;
+        case 5:
+            file = fopen("long.txt","r");
+            x = rand()%100;
+            for(int i=0;i<10;i++){
+                for(int j=0;j<x;j++){
+                    fscanf(file,"%s", word);
+                }
+                addWord(word);
+                x = rand()%100;
+                rewind(file);
+            }
+            fclose(file);
+            break;
+        default:
+            return;
+            break;
+    }
 }
 ////////////////////////////////////////////////////////////
 void createFilesOfWords(){
@@ -584,4 +774,5 @@ void addWord(char word[]){
 void deleteWord(){
     head = head->next;
     countOfWords--;
+    y--;
 }
